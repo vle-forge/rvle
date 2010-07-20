ebf206b1303a440f38eed6729a83e026aedaa8e8
Patrick Chabrier <patrick.chabrier@toulouse.inra.fr>
2010-07-20 12:58:21 +0200

    Add an Object-Oriented Framework

---
+++ R/rvleClass.R
@@ -0,0 +1,314 @@
+#
+# @file rvle.R
+# @author The VLE Development Team
+#
+
+#
+# VLE Environment - the multimodeling and simulation environment
+# This file is a part of the VLE environment (http://vle.univ-littoral.fr)
+# Copyright (C) 2003 - 2008 The VLE Development Team
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+conditionMultiSetBoolean<- function(object, conditioname, portname, value) {
+  rvle.clearConditionPort(object@sim, conditioname, portname)
+
+  lapply(value,
+         function(item, object, conditioname, portname) {
+           rvle.addBooleanCondition(object@sim, conditioname, portname, item)
+         },
+         object,
+         conditioname,
+         portname)
+}
+
+conditionMultiSetReal<- function(object, conditioname, portname, value) {
+  rvle.clearConditionPort(object@sim, conditioname, portname)
+
+  lapply(value,
+         function(item, object, conditioname, portname) {
+           rvle.addRealCondition(object@sim, conditioname, portname, item)
+         },
+         object,
+         conditioname,
+         portname)
+}
+
+conditionMultiSetInteger<- function(object, conditioname, portname, value) {
+  rvle.clearConditionPort(object@sim, conditioname, portname)
+
+  lapply(value,
+         function(item, object, conditioname, portname) {
+           rvle.addIntegerCondition(object@sim, conditioname, portname, item)
+         },
+         object,
+         conditioname,
+         portname)
+}
+
+conditionMultiSetString<- function(object, conditioname, portname, value) {
+  rvle.clearConditionPort(object@sim, conditioname, portname)
+
+  lapply(value,
+         function(item, object, conditioname, portname) {
+           rvle.addStringCondition(object@sim, conditioname, portname, item)
+         },
+         object,
+         conditioname,
+         portname)
+}
+
+switchPlan <- function(object, seed = 12345678) {
+  stopifnot(is.rvle(object@sim))
+
+  switch(object@config$plan,
+         single = if (object@config$replicas == 1) {
+           rvle.setSeed(object@sim, seed)
+         } else {
+           rvle.setLinearCombination(object@sim, seed, object@config$replicas)
+         },
+         linear = rvle.setLinearCombination(object@sim, seed, object@config$replicas),
+         total = rvle.setTotalCombination(object@sim, seed, object@config$replicas))
+}
+
+switchRun <- function(object) {
+  stopifnot(is.rvle(object@sim))
+
+  if (object@config$plan == "single") {
+    object@outlist <- switch(object@config$restype,
+                             dataframe = rvle.run(object@sim),
+                             matrix = rvle.runMatrix(object@sim))
+  } else {
+    object@outmatrix <- switch(object@config$proc,
+                               mono = switch(object@config$restype,
+                                 dataframe = rvle.runManager(object@sim),
+                                 matrix = rvle.runManagerMatrix(object@sim)),
+                               thread = switch(object@config$restype,
+                                 dataframe = rvle.runManagerThread(object@sim,object@config$thread),
+                                 matrix = rvle.runManagerThreadMatrix(object@sim), object@config$thread),
+                               cluster = switch(object@config$restype,
+                                 dataframe = rvle.runManagerCluster(object@sim),
+                                 matrix = rvle.runManagerClusterMatrix(object@sim)))}
+
+  object@run <- setRun(object)
+
+  invisible(object)
+}
+
+setRun <- function(object) {
+  ## container of the argument list
+  alis <- alist(object=)
+  ## list container of the body
+  exprlist <- list()
+
+  conditionlist <- rvle.listConditions(object@sim)
+  conditionportlist <- lapply(conditionlist,
+                              function(condition) {
+                                list(condition,
+                                     rvle.listConditionPorts(object@sim,
+							     condition))
+                              })
+
+  for (i in 1:length(conditionportlist)) {
+
+    conditioname <- conditionportlist[[i]][[1]]
+    portlist <- conditionportlist[[i]][[2]]
+    portname = portlist[[1]]
+
+
+    for (j in 1:length(portlist)) {
+      portname = portlist[[j]]
+
+      thevalue = rvle.getConditionPortValues(object@sim,
+                                             conditioname,
+                                             portname)
+
+      ## ajout de la commande d'affectation
+      if (length(thevalue) != 0) {
+        cscall <- switch(storage.mode(thevalue),
+                         logical = list(conditionMultiSetBoolean,
+                           quote(object),
+                           conditioname,
+                           portname,
+                           as.symbol(sprintf("%s.%s",conditioname,portname))),
+                         double = list(conditionMultiSetReal,
+                           quote(object),
+                           conditioname,
+                           portname,
+                           as.symbol(sprintf("%s.%s",conditioname,portname))),
+                         integer = list(conditionMultiSetInteger,
+                           quote(object),
+                           conditioname,
+                           portname,
+                           as.symbol(sprintf("%s.%s",conditioname,portname))),
+                         character = list(conditionMultiSetString,
+                           quote(object),
+                           conditioname,
+                           portname,
+                           as.symbol(sprintf("%s.%s",conditioname,portname)))
+                         )
+
+        mode(cscall) <- "call"
+        exprlist <- c(exprlist,cscall)
+
+        ##ajout d'un argument
+        listuna <- list(thevalue)
+        names(listuna)[1] <- sprintf("%s.%s",
+                                     conditioname,
+                                     portname)
+        alis <- c(alis, listuna)
+      }
+    }
+  }
+
+  ## to add seed, duration and replicas to the argument list
+  alis$seed <- rvle.getSeed(object@sim)
+  alis$duration <- rvle.getDuration(object@sim)
+
+  formals(object@run) <- alis
+
+  ## to add the set_seed, set_replicas, set_duration, and run
+  ## to close and to set the body of the simulator
+  cscall = call("switchPlan", quote(object), quote(seed))
+  exprlist <- c(exprlist,cscall)
+
+  cscall = call("rvle.setDuration", quote(object@sim), quote(duration))
+  exprlist <- c(exprlist,cscall)
+
+  cscall = call("switchRun", quote(object))
+
+  exprlist <- c(exprlist,cscall)
+
+  mode(exprlist) <- "expression"
+  finalcall <- list(eval, exprlist)
+  mode(finalcall) <- "call"
+
+  body(object@run) <- finalcall
+
+  fun <- function(object) standardGeneric("run")
+  formals(fun) <- alis
+  setGeneric("run", fun)
+
+  setMethod("run",
+            "Rvle",
+            object@run)
+
+  object@run
+}
+
+setClass("rvle")
+
+setClass("Rvle", representation(sim = "rvle",
+                                file = "character",
+                                run = "function",
+                                config = "list",
+                                outlist = "list",
+                                outmatrix = "matrix"))
+
+setMethod("initialize",
+          "Rvle",
+          function(.Object, file = character(length = 0)) {
+            .Object@file <- file
+            .Object@sim  <- rvle.open(.Object@file)
+            .Object@run <- function() {}
+            .Object@config <- list(plan = "single", proc = "mono", restype = "dataframe", thread = 1, replicas = 1)
+
+            .Object@run <- setRun(.Object)
+            .Object
+          })
+
+setGeneric("config", function(object) standardGeneric("config"))
+setGeneric("config<-", function(object, value) standardGeneric("config<-"))
+
+setMethod("config", "Rvle", function(object) { object@config })
+setMethod("config<-", "Rvle",
+          function(object,
+                   value) {
+            object@config <- value
+            object@outlist <- list()
+            invisible(object)
+          })
+
+setGeneric("results", function(object) standardGeneric("results"))
+setMethod("results", "Rvle",
+          function(object) {
+            if (object@config$plan == "single") {
+              object@outlist
+            } else {
+              object@outmatrix
+            }})
+
+setGeneric("saveVpz", function(object, file) standardGeneric("saveVpz"))
+setMethod("saveVpz", "Rvle",
+          function(object, file) {
+            rvle.save(object@sim, file)
+            invisible(object)
+            })
+
+if (!isGeneric("show")) {
+  if (is.function("show"))
+    fun <- show
+  else fun <- function(object) standardGeneric("show")
+  setGeneric("show", fun)
+}
+
+setMethod("show",
+          "Rvle",
+          function(object) {
+
+            cat("VLE Model informations :\n")
+            cat("========================\n")
+            cat("\n")
+            cat("File name : ", object@file, "\n")
+            cat("\n")
+            cat("Duration : ", rvle.getDuration(object@sim), "\n")
+            cat("\n")
+            cat("Seed : ", rvle.getSeed(object@sim), "\n")
+            cat("\n")
+
+            cat("Experimental Condition list and default value :\n")
+
+            conditionlist <- rvle.listConditions(object@sim)
+
+            conditionportlist <- lapply(conditionlist,
+                                        function(condition) {
+                        list(condition, rvle.listConditionPorts(object@sim,
+                                                                condition))
+                                        })
+
+            lapply(conditionportlist, function(condition) {
+              conditioname <- condition[[1]]
+              lapply(condition[[2]], function(portname) {
+                thevalue = rvle.getConditionPortValues(object@sim,
+                                                       conditioname,
+                                                       portname)
+                if (length(thevalue) == 0) {
+                  thestoragemode = ""
+                  thevalue = "Experimental Condition not managed"
+                }
+                else {
+                  thestoragemode = sprintf("<%s>", storage.mode(thevalue));
+                }
+                cat(sprintf("* %s.%s = %s %s\n",
+                            conditioname,
+                            portname,
+                            toString(thevalue),
+                            thestoragemode))
+              })
+            })
+            cat("\n")
+            return(invisible())
+          })
+
---
+++ tests/sirO.R
@@ -0,0 +1,89 @@
+library(RUnit)
+library(rvle)
+
+sir <- new("Rvle", file = "sir.vpz")
+
+arglist <- as.pairlist(alist(object=,
+                             cond_sir.a = 0.5, cond_sir.active = FALSE, cond_sir.dependance = FALSE,
+                             cond_sir.r = 0.005, cond_sir.threshold = 1e-10, seed = 12379843L,
+                             duration = 20))
+
+checkEquals(arglist, formals(run))
+
+
+# simple run and direct access to the result
+result <- run(sir)@outlist[[1]]
+
+# check result of the view
+checkEqualsNumeric(dim(result)[1], 2000)
+checkEqualsNumeric(dim(result)[2], 4)
+
+# simple run and keep
+sir <- run(sir)
+result <- results(sir)[[1]]
+
+# check result of the view
+checkEqualsNumeric(dim(result)[1], 2000)
+checkEqualsNumeric(dim(result)[2], 4)
+
+# get the first view
+view1 <- result
+checkEqualsNumeric(view1$time[[2000]], 19.99, tolerance=1e-5)
+checkEqualsNumeric(view1$":sir.I"[[2000]], 0.1542165, tolerance=1e-5)
+checkEqualsNumeric(view1$":sir.R"[[2000]], 4.973883e+02, tolerance=1e-5)
+checkEqualsNumeric(view1$":sir.S"[[2000]], 3.457524, tolerance=1e-5)
+
+# change condition
+result <- run(sir, cond_sir.a = 0.6)@outlist[[1]]
+
+checkEqualsNumeric(dim(result)[1], 2000)
+checkEqualsNumeric(dim(result)[2], 4)
+
+view1 <- result
+checkEqualsNumeric(view1$time[[2000]], 19.99, tolerance=1e-5)
+checkEqualsNumeric(view1$":sir.I"[[2000]], 0.04369215, tolerance=1e-5)
+checkEqualsNumeric(view1$":sir.R"[[2000]], 4.927206e+02, tolerance=1e-5)
+checkEqualsNumeric(view1$":sir.S"[[2000]], 8.235717, tolerance=1e-5)
+
+# the new list of args
+sir <- run(sir, cond_sir.a = 0.6)
+
+arglist <- as.pairlist(alist(object=,
+                             cond_sir.a = 0.6,
+			     cond_sir.active = FALSE,
+			     cond_sir.dependance = FALSE,
+                             cond_sir.r = 0.005,
+			     cond_sir.threshold = 1e-10,
+			     seed = 12379843L,
+                             duration = 20))
+
+checkEquals(arglist, formals(run))
+
+# check the storage of the result
+config(sir)["restype"] <- "matrix"
+config(sir)$restype  <- "matrix"
+checkTrue(is.matrix(run(sir, duration = 0.25)@outlist[[1]]))
+
+config(sir)["restype"] <- "dataframe"
+config(sir)$restype  <- "dataframe"
+checkTrue(is.data.frame(run(sir, duration = 0.25)@outlist[[1]]))
+
+
+# check many experimental plans with small duration
+config(sir)["plan"] <- "linear"
+sir <- run(sir, cond_sir.a = c(0.6, 0.5), cond_sir.r = c(0.005, 0.004), duration = 0.25)
+
+result <- results(sir)
+checkEquals(dim(result), c(1,2))
+
+config(sir)["replicas"] <- 2
+sir <- run(sir, cond_sir.a = c(0.6, 0.5), cond_sir.r = c(0.005, 0.004), duration = 0.25)
+
+result <- results(sir)
+checkEquals(dim(result), c(2,2))
+
+config(sir)["plan"] <- "total"
+sir <- run(sir, cond_sir.a = c(0.6, 0.5), cond_sir.r = c(0.005, 0.004), duration = 0.25)
+
+result <- results(sir)
+checkEquals(dim(result), c(2,4))
